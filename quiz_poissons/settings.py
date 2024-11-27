956267c3c5fc22b1db5e911e72a10af0f611cc01
StalloneDev <139977747+StalloneDev@users.noreply.github.com>
2024-11-27 23:31:52 +0100

    Update settings.py

--- quiz_poissons/settings.py
+++ quiz_poissons/settings.py
@@ -28,7 +28,6 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'whitenoise.runserver_nostatic',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
@@ -37,7 +36,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
